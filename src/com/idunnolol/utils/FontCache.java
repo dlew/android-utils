cb54d066ea67d0151076a457425f74ba99d3aded
Daniel Lew <danlew42@gmail.com>
2013-05-09 20:00:27 -0500

    Added simple FontCache

---
+++ src/com/idunnolol/utils/FontCache.java
@@ -0,0 +1,25 @@
+package com.idunnolol.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Simple font cache, keeps us from having to reload the same font over
+ * and over again.
+ */
+public class FontCache {
+
+	private static Map<String, Typeface> sCachedFonts = new HashMap<String, Typeface>();
+
+	public static Typeface getTypeface(Context context, String assetPath) {
+		if (!sCachedFonts.containsKey(assetPath)) {
+			Typeface tf = Typeface.createFromAsset(context.getAssets(), assetPath);
+			sCachedFonts.put(assetPath, tf);
+		}
+
+		return sCachedFonts.get(assetPath);
+	}
+}
